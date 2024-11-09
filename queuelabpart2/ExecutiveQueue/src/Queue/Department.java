dc301c1f43e8e3b555f1d2e3b56c83e1138fbef1
RailRooooooad <117558213+RailRooooooad@users.noreply.github.com>
2024-11-08 23:58:43 -0500

    Update Department.java

--- queuelabpart2/ExecutiveQueue/src/Queue/Department.java
+++ queuelabpart2/ExecutiveQueue/src/Queue/Department.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+
 class Department {
     private String name;
     private Queue<Executive> executives;
